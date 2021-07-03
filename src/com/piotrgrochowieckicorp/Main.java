ed2c5b2ad03bef1d93ffc6d45fff3f074e6b30a8
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2021-07-03 19:30:36 +0200

    Added sentence about CFD career.

--- src/com/piotrgrochowieckicorp/Main.java
+++ src/com/piotrgrochowieckicorp/Main.java
@@ -5,7 +5,10 @@ public class Main {
     public static void main(String[] args) {
         int age = 26;
         int dog_age = 8;
+        int cfd_exp = 2;
+        String cfd = "Currently I am an CFD Engineer with ";
         String dog_sentence = "I have a dog. The dog is called Lucy and she's ";
 	    System.out.println("Hello World! My name is Piotr and I am " + age + "." + dog_sentence + dog_age);
+	    System.out.println(cfd + cfd_exp + " years of experience in industry. At the moment I am learning programming :)");
     }
 }

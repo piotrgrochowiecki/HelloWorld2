e0386810a85e6c1f134bfb8e675fa172965a0aa2
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2021-07-03 19:09:36 +0200

    Changed dog's age from 4 to 8.

--- src/com/piotrgrochowieckicorp/Main.java
+++ src/com/piotrgrochowieckicorp/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         int age = 26;
-        int dog_age = 4;
+        int dog_age = 8;
         String dog_sentence = "I have a dog. The dog is called Lucy and she's ";
 	    System.out.println("Hello World! My name is Piotr and I am " + age + "." + dog_sentence + dog_age);
     }

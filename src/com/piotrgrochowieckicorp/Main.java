6adad82cb4bab27adc06195fadb007c9c7b8165d
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2021-07-03 19:01:26 +0200

    First lines of code. Learning Git and GitHub

--- src/com/piotrgrochowieckicorp/Main.java
+++ src/com/piotrgrochowieckicorp/Main.java
@@ -3,6 +3,7 @@ package com.piotrgrochowieckicorp;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello World! My name is Piotr");
+        int age = 26;
+	    System.out.println("Hello World! My name is Piotr and I am " + age);
     }
 }
